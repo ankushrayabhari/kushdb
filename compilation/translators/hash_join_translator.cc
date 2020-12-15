c944f192dba880a44fd2a405d2776a4e11d2d8b7
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2020-12-15 13:36:27 -0800

    Finish probe side of hash join

--- compilation/translators/hash_join_translator.cc
+++ compilation/translators/hash_join_translator.cc
@@ -29,6 +29,7 @@ void HashJoinTranslator::Consume(OperatorTranslator& src) {
   auto& left_translator = LeftChild();
   auto& right_translator = RightChild();
 
+  // Build side
   if (&src == &left_translator) {
     // TODO: hash key
     auto bucket_var = program.GenerateVariable();
@@ -41,53 +42,48 @@ void HashJoinTranslator::Consume(OperatorTranslator& src) {
     for (const auto& [variable, type] : left_translator.GetValues().Values()) {
       program.fout << bucket_var << ".push_back(" << variable << ");\n";
     }
-  } else {
-    /*
-    auto& right_child = hash_join.RightChild();
-    std::string key_var = translator_.context_.GetOutputVariables(
-        right_child)[hash_join.right_column_->GetColumnIdx()];
 
-    // probe hash table and start outputting tuples
-    auto bucket_var = GenerateVar();
-    out << "auto& " << bucket_var << " = " << buffer_var << "[" << key_var
-        << "];\n";
-
-    const auto& left_schema = hash_join.LeftChild().Schema().Columns();
-
-    auto loop_var = GenerateVar();
-    out << "for (int " << loop_var << " = 0; " << loop_var << " < "
-        << bucket_var << ".size(); " << loop_var << "+= " << left_schema.size()
-        << "){\n";
-
-    std::vector<std::string> column_variables;
-
-    // TODO: unpack tuples from hash table
-    int i = 0;
-    for (const auto& left_column : left_schema) {
-      std::string var = GenerateVar();
-      std::string type = SqlTypeToRuntimeType(left_column.Type());
-
-      out << type << " " << var << " = " << bucket_var << "[" << loop_var
-          << "+ " << i++ << "];\n";
-
-      column_variables.push_back(var);
-    }
-
-    for (const auto& var :
-         translator_.context_.GetOutputVariables(hash_join.RightChild())) {
-      column_variables.push_back(var);
-    }
-
-    translator_.context_.SetOutputVariables(hash_join,
-                                            std::move(column_variables));
-
-    auto parent = hash_join.Parent();
-    if (parent.has_value()) {
-      translator_.consumer_.Consume(parent.value(), hash_join);
-    }
-
-    out << "}\n"; */
+    return;
   }
+
+  // Probe Side
+  auto bucket_var = program.GenerateVariable();
+  program.fout << "auto& " << bucket_var << " = " << hash_table_var_ << "[";
+  ExpressionTranslator key_translator(context_, right_translator);
+  key_translator.Produce(hash_join_.RightColumn());
+  program.fout << "];\n";
+
+  const auto& left_schema_values = left_translator.GetValues().Values();
+
+  auto loop_var = program.GenerateVariable();
+  program.fout << "for (int " << loop_var << " = 0; " << loop_var << " < "
+               << bucket_var << ".size(); " << loop_var
+               << "+= " << left_schema_values.size() << "){\n";
+
+  std::vector<std::string> column_variables;
+
+  // TODO: unpack tuples from hash table
+  int i = 0;
+  for (const auto& [variable, type] : left_schema_values) {
+    program.fout << type << "& " << variable << " = " << bucket_var << "["
+                 << loop_var << "+ " << i++ << "];\n";
+  }
+
+  SchemaValues values;
+  for (const auto& [variable, type] : left_schema_values) {
+    values.AddVariable(variable, type);
+  }
+  const auto& right_schema_values = right_translator.GetValues().Values();
+  for (const auto& [variable, type] : right_schema_values) {
+    values.AddVariable(variable, type);
+  }
+  SetSchemaValues(std::move(values));
+
+  if (auto parent = Parent()) {
+    parent->get().Consume(*this);
+  }
+
+  program.fout << "}\n";
 }
 
 }  // namespace kush::compile
