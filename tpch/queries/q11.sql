b44e3c82cf3f922d9766894122c18634c148f45f
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2020-12-18 12:49:07 -0800

    Add sql representations for postgres comparison

---
+++ tpch/queries/q11.sql
@@ -0,0 +1,13 @@
+select
+	ps_partkey,
+	sum(ps_supplycost * ps_availqty) as value
+from
+	partsupp,
+	supplier,
+	nation
+where
+	ps_suppkey = s_suppkey
+	and s_nationkey = n_nationkey
+	and n_name = 'ARGENTINA'
+group by
+	ps_partkey;
---
+++ tpch/queries/q12.sql
@@ -0,0 +1,12 @@
+select
+	l_shipmode,
+from
+	lineitem
+where
+	(l_shipmode = 'FOB' or l_shipmode = 'SHIP') 
+    and l_commitdate < l_receiptdate 
+    and l_shipdate < l_commitdate 
+    and l_receiptdate >= date '1994-01-01' 
+    and l_receiptdate < '1995-01-01'
+group by
+	l_shipmode;
