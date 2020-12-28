e7bfdeebc5d215b6ed047e0e823ff212e4ab11c6
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2020-12-28 11:33:56 -0800

    Fix header includes

--- compile/llvm/llvm_program.cc
+++ compile/llvm/llvm_program.cc
@@ -2,9 +2,9 @@
 
 #include <memory>
 
-#include "llvm/include/llvm/IR/IRBuilder.h"
-#include "llvm/include/llvm/IR/LLVMContext.h"
-#include "llvm/include/llvm/IR/Module.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
 
 namespace kush::compile {
 
--- compile/llvm/llvm_program.h
+++ compile/llvm/llvm_program.h
@@ -3,9 +3,9 @@
 #include <memory>
 
 #include "compile/program.h"
-#include "llvm/include/llvm/IR/IRBuilder.h"
-#include "llvm/include/llvm/IR/LLVMContext.h"
-#include "llvm/include/llvm/IR/Module.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
 
 namespace kush::compile {
 
