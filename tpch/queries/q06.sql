8ef1963c8a48629b87fb0de49888d3d41dd8d941
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2020-12-28 15:38:49 -0800

    Copy TPCH queries

--- tpch/queries/q06.sql
+++ tpch/queries/q06.sql
@@ -1,12 +1,1 @@
-select
-	sum(l_extendedprice * l_discount) as revenue
-from
-	lineitem_06
-where
-	l_shipdate >= date '1994-01-01'
-	and l_shipdate < date '1995-01-01'
-	and l_discount >= 0.07 
-    and l_discount <= 0.09 
-	and l_quantity < 24
-group by 
-	true;
+select sum(l_extendedprice * l_discount) as revenue from lineitem where l_shipdate >= date '1994-01-01' and l_shipdate < date '1994-01-01' + interval '1' year and l_discount between 0.08 - 0.01 and 0.08 + 0.01 and l_quantity < 24;
--- tpch/queries/q11.sql
+++ tpch/queries/q11.sql
@@ -1,13 +1,1 @@
-select
-	ps_partkey,
-	sum(ps_supplycost * ps_availqty) as value
-from
-	partsupp_11,
-	supplier_11,
-	nation_11
-where
-	ps_suppkey = s_suppkey
-	and s_nationkey = n_nationkey
-	and n_name = 'ARGENTINA'
-group by
-	ps_partkey;
+select ps_partkey, sum(ps_supplycost * ps_availqty) as value from partsupp, supplier, nation where ps_suppkey = s_suppkey and s_nationkey = n_nationkey and n_name = 'ARGENTINA' group by ps_partkey having sum(ps_supplycost * ps_availqty) > ( select sum(ps_supplycost * ps_availqty) * 0.0001000000 from partsupp, supplier, nation where ps_suppkey = s_suppkey and s_nationkey = n_nationkey and n_name = 'ARGENTINA' ) order by value desc;
