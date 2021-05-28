4eaef272a40f6787313b9a1ab3c35ed564838b05
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2021-05-27 19:59:11 -0700

    Convert proxy::If to generate KHIR

--- compile/proxy/if.cc
+++ compile/proxy/if.cc
@@ -3,36 +3,18 @@
 #include <functional>
 #include <utility>
 
-#include "compile/ir_registry.h"
-#include "compile/program_builder.h"
+#include "compile/khir/khir_program_builder.h"
 #include "compile/proxy/bool.h"
 
 namespace kush::compile::proxy {
 
-template <typename T>
-If<T>::If(ProgramBuilder<T>& program, const Bool<T>& cond,
-          std::function<void()> then_fn)
-    : program_(program), b1(program.CurrentBlock()) {
-  auto dest_block = program.GenerateBlock();
-  auto then_block = program.GenerateBlock();
-
-  program.Branch(cond.Get(), then_block, dest_block);
-
-  program.SetCurrentBlock(then_block);
-  then_fn();
-  if (!program_.IsTerminated(program_.CurrentBlock())) {
-    b2 = program_.CurrentBlock();
-    program.Branch(dest_block);
-  }
-
-  program.SetCurrentBlock(dest_block);
-}
-
-template <typename T>
-If<T>::If(ProgramBuilder<T>& program, const Bool<T>& cond,
-          std::function<void()> then_fn, std::function<void()> else_fn)
-    : program_(program) {
-  std::optional<typename ProgramBuilder<T>::BasicBlock> dest_block;
+std::vector<khir::Value> If(khir::KHIRProgramBuilder& program, const Bool& cond,
+                            std::function<std::vector<khir::Value>()> then_fn,
+                            std::function<std::vector<khir::Value>()> else_fn) {
+  std::vector<khir::Type> phi_types;
+  std::vector<khir::Value> then_branch_phi_members;
+  std::vector<khir::Value> else_branch_phi_members;
+  std::optional<khir::BasicBlockRef> dest_block;
 
   auto first_block = program.GenerateBlock();
   auto second_block = program.GenerateBlock();
@@ -40,32 +22,50 @@ If<T>::If(ProgramBuilder<T>& program, const Bool<T>& cond,
   program.Branch(cond.Get(), first_block, second_block);
 
   program.SetCurrentBlock(first_block);
-  then_fn();
-  if (!program_.IsTerminated(program_.CurrentBlock())) {
+  const auto then_branch_values = then_fn();
+  if (!program.IsTerminated(program.CurrentBlock())) {
     if (!dest_block.has_value()) {
       dest_block = program.GenerateBlock();
     }
 
-    b1 = program_.CurrentBlock();
+    for (auto v : then_branch_values) {
+      then_branch_phi_members.push_back(program.PhiMember(v));
+      phi_types.push_back(program.TypeOf(v));
+    }
     program.Branch(*dest_block);
   }
 
   program.SetCurrentBlock(second_block);
-  else_fn();
-  if (!program_.IsTerminated(program_.CurrentBlock())) {
+  const auto else_branch_values = else_fn();
+
+  if (!program.IsTerminated(program.CurrentBlock())) {
     if (!dest_block.has_value()) {
       dest_block = program.GenerateBlock();
     }
 
-    b2 = program_.CurrentBlock();
+    for (auto v : else_branch_values) {
+      else_branch_phi_members.push_back(program.PhiMember(v));
+    }
     program.Branch(*dest_block);
   }
 
   if (dest_block != std::nullopt) {
     program.SetCurrentBlock(*dest_block);
+
+    std::vector<khir::Value> phi_values;
+    for (int i = 0; i < phi_types.size(); i++) {
+      phi_values.push_back(program.Phi(phi_types[i]));
+    }
+    for (int i = 0; i < then_branch_phi_members.size(); i++) {
+      program.UpdatePhiMember(phi_values[i], then_branch_phi_members[i]);
+    }
+    for (int i = 0; i < else_branch_phi_members.size(); i++) {
+      program.UpdatePhiMember(phi_values[i], else_branch_phi_members[i]);
+    }
+    return phi_values;
+  } else {
+    return {};
   }
 }
-
-INSTANTIATE_ON_IR(If);
 
 }  // namespace kush::compile::proxy
--- compile/proxy/if.h
+++ compile/proxy/if.h
@@ -3,82 +3,13 @@
 #include <functional>
 #include <utility>
 
-#include "compile/program_builder.h"
+#include "compile/khir/khir_program_builder.h"
 #include "compile/proxy/bool.h"
-#include "compile/proxy/float.h"
-#include "compile/proxy/int.h"
-#include "compile/proxy/string.h"
 
 namespace kush::compile::proxy {
 
-template <typename T>
-class If {
- public:
-  If(ProgramBuilder<T>& program, const Bool<T>& cond,
-     std::function<void()> then_fn);
-  If(ProgramBuilder<T>& program, const Bool<T>& cond,
-     std::function<void()> then_fn, std::function<void()> else_fn);
-
-  If(const If<T>&) = delete;
-  If<T>& operator=(const If<T>&) = delete;
-  If(If<T>&&) = delete;
-  If<T>& operator=(If<T>&&) = delete;
-
-  proxy::Bool<T> Phi(const proxy::Bool<T>& v1, const proxy::Bool<T>& v2) {
-    return PhiImpl<proxy::Bool<T>>(v1, v2);
-  }
-
-  proxy::Float64<T> Phi(const proxy::Float64<T>& v1,
-                        const proxy::Float64<T>& v2) {
-    return PhiImpl<proxy::Float64<T>>(v1, v2);
-  }
-
-  proxy::Int8<T> Phi(const proxy::Int8<T>& v1, const proxy::Int8<T>& v2) {
-    return PhiImpl<proxy::Int8<T>>(v1, v2);
-  }
-
-  proxy::Int16<T> Phi(const proxy::Int16<T>& v1, const proxy::Int16<T>& v2) {
-    return PhiImpl<proxy::Int16<T>>(v1, v2);
-  }
-
-  proxy::Int32<T> Phi(const proxy::Int32<T>& v1, const proxy::Int32<T>& v2) {
-    return PhiImpl<proxy::Int32<T>>(v1, v2);
-  }
-
-  proxy::Int64<T> Phi(const proxy::Int64<T>& v1, const proxy::Int64<T>& v2) {
-    return PhiImpl<proxy::Int64<T>>(v1, v2);
-  }
-
-  proxy::String<T> Phi(const proxy::String<T>& v1, const proxy::String<T>& v2) {
-    return PhiImpl<proxy::String<T>>(v1, v2);
-  }
-
- private:
-  template <typename S>
-  S PhiImpl(const S& v1, const S& v2) {
-    if (!b1.has_value() || !b2.has_value()) {
-      throw std::runtime_error("Don't call phi on a single predecessor block.");
-    }
-    typename ProgramBuilder<T>::Value phi =
-        program_.Phi(program_.TypeOf(v1.Get()));
-    program_.AddToPhi(phi, v1.Get(), b1.value());
-    program_.AddToPhi(phi, v2.Get(), b2.value());
-    return S(program_, phi);
-  }
-
-  ProgramBuilder<T>& program_;
-  std::optional<typename ProgramBuilder<T>::BasicBlock> b1;
-  std::optional<typename ProgramBuilder<T>::BasicBlock> b2;
-};
-
-template <typename T, typename S>
-S Ternary(ProgramBuilder<T>& program, const proxy::Bool<T>& cond,
-          std::function<S()> left, std::function<S()> right) {
-  std::unique_ptr<S> l, r;
-  If<T> check(
-      program, cond, [&]() { l = left().ToPointer(); },
-      [&]() { r = right().ToPointer(); });
-  return check.Phi(*l, *r);
-}
+std::vector<khir::Value> If(khir::KHIRProgramBuilder& program, const Bool& cond,
+                            std::function<std::vector<khir::Value>()> then_fn,
+                            std::function<std::vector<khir::Value>()> else_fn);
 
 }  // namespace kush::compile::proxy
