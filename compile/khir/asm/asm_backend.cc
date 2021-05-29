9217ae136c007d4c0d680ff9edf2a50d6f2ea65f
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2021-05-29 16:42:21 -0700

    Add empty ASM backend

---
+++ compile/khir/asm/asm_backend.cc
@@ -0,0 +1,22 @@
+#include "compile/khir/asm/asm_backend.h"
+
+#include "absl/types/span.h"
+
+#include "compile/khir/instruction.h"
+#include "compile/khir/program_builder.h"
+#include "compile/khir/type_manager.h"
+
+namespace kush::khir {
+
+// Instructions
+void ASMBackend::TranslateFuncDecl(bool pub, bool external,
+                                   std::string_view name, Type function_type) {}
+
+void ASMBackend::TranslateFuncBody(
+    int func_idx, const std::vector<uint64_t>& i64_constants,
+    const std::vector<double>& f64_constants,
+    const std::vector<int>& basic_block_order,
+    const std::vector<std::pair<int, int>>& basic_blocks,
+    const std::vector<uint64_t>& instructions) {}
+
+}  // namespace kush::khir
---
+++ compile/khir/asm/asm_backend.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "compile/khir/program_builder.h"
+#include "compile/khir/type_manager.h"
+
+namespace kush::khir {
+
+class ASMBackend : public Backend {
+ public:
+  ASMBackend();
+  virtual ~ASMBackend() = default;
+
+  // Types
+  void TranslateVoidType() override;
+  void TranslateI1Type() override;
+  void TranslateI8Type() override;
+  void TranslateI16Type() override;
+  void TranslateI32Type() override;
+  void TranslateI64Type() override;
+  void TranslateF64Type() override;
+  void TranslatePointerType(Type elem) override;
+  void TranslateArrayType(Type elem, int len) override;
+  void TranslateFunctionType(Type result,
+                             absl::Span<const Type> arg_types) override;
+  void TranslateStructType(absl::Span<const Type> elem_types) override;
+
+  // Globals
+  void TranslateGlobalConstCharArray(std::string_view s) override;
+  void TranslateGlobalStruct(bool constant, Type t,
+                             absl::Span<const uint64_t> v,
+                             const std::vector<uint64_t>& i64_constants,
+                             const std::vector<double>& f64_constants) override;
+  void TranslateGlobalArray(bool constant, Type t, absl::Span<const uint64_t> v,
+                            const std::vector<uint64_t>& i64_constants,
+                            const std::vector<double>& f64_constants) override;
+  void TranslateGlobalPointer(
+      bool constant, Type t, uint64_t v,
+      const std::vector<uint64_t>& i64_constants,
+      const std::vector<double>& f64_constants) override;
+
+  // Instructions
+  void TranslateFuncDecl(bool pub, bool external, std::string_view name,
+                         Type function_type) override;
+  void TranslateFuncBody(int func_idx,
+                         const std::vector<uint64_t>& i64_constants,
+                         const std::vector<double>& f64_constants,
+                         const std::vector<int>& basic_block_order,
+                         const std::vector<std::pair<int, int>>& basic_blocks,
+                         const std::vector<uint64_t>& instructions) override;
+
+  // Program
+  void Compile() const override;
+  void Execute() const override;
+
+ private:
+  mutable std::chrono::time_point<std::chrono::system_clock> start, gen, comp,
+      link, end;
+};
+
+}  // namespace kush::khir
