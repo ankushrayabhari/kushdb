8d2b73b2c952217c9fc4d4f5be3e0e47a0a36e27
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2021-08-22 19:47:23 -0700

    Switch seed to random

--- runtime/skinner_join_executor.cc
+++ runtime/skinner_join_executor.cc
@@ -559,7 +559,7 @@ class UctNode {
         num_tries_per_action_(num_actions_, 0),
         acc_reward_per_action_(num_actions_, 0),
         table_per_action_(num_actions_),
-        rng_(2) {
+        rng_(std::chrono::system_clock::now().time_since_epoch().count()) {
     for (int i = 0; i < num_actions_; i++) {
       priority_actions_.push_back(i);
       recommended_actions_.insert(i);
@@ -584,7 +584,7 @@ class UctNode {
         joined_tables_(parent.joined_tables_),
         unjoined_tables_(parent.unjoined_tables_),
         table_per_action_(num_actions_),
-        rng_(2) {
+        rng_(std::chrono::system_clock::now().time_since_epoch().count()) {
     joined_tables_.insert(joined_table);
 
     auto it = std::find(unjoined_tables_.begin(), unjoined_tables_.end(),
