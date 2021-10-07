e749327a0c72a6649a645c717f17d75355e30a12
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2021-10-07 13:59:23 -0400

    Add JCCH queries

---
+++ jcch/queries/q18.sql
@@ -0,0 +1,35 @@
+select
+	c_name,
+	c_custkey,
+	o_orderkey,
+	o_orderdate,
+	o_totalprice,
+	sum(l_quantity)
+from
+	customer,
+	orders,
+	lineitem
+where
+	o_orderkey in (
+		select
+			l_orderkey
+		from
+			lineitem
+		where
+			l_quantity <= 50
+		group by
+			l_orderkey having
+				sum(l_quantity) > 312
+	)
+	and c_custkey = o_custkey
+	and o_orderkey = l_orderkey
+group by
+	c_name,
+	c_custkey,
+	o_orderkey,
+	o_orderdate,
+	o_totalprice
+order by
+	o_totalprice desc,
+	o_orderdate;
+first 100
