4f39087ccf989173a62dff7f7202f8a954e94676
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2021-10-08 21:21:53 -0400

    Add methods to ScanTranslator to support skinner opts

--- compile/translators/scan_translator.cc
+++ compile/translators/scan_translator.cc
@@ -25,86 +25,85 @@ ScanTranslator::ScanTranslator(
       scan_(scan),
       program_(program) {}
 
-void ScanTranslator::Produce() {
+std::unique_ptr<proxy::Iterable> ScanTranslator::ConstructIterable(
+    catalog::SqlType type, std::string_view path) {
+  using SqlType = catalog::SqlType;
+  switch (type) {
+    case SqlType::SMALLINT:
+      return std::make_unique<proxy::ColumnData<SqlType::SMALLINT>>(program_,
+                                                                    path);
+    case SqlType::INT:
+      return std::make_unique<proxy::ColumnData<SqlType::INT>>(program_, path);
+    case SqlType::BIGINT:
+      return std::make_unique<proxy::ColumnData<SqlType::BIGINT>>(program_,
+                                                                  path);
+    case SqlType::REAL:
+      return std::make_unique<proxy::ColumnData<SqlType::REAL>>(program_, path);
+    case SqlType::DATE:
+      return std::make_unique<proxy::ColumnData<SqlType::DATE>>(program_, path);
+    case SqlType::TEXT:
+      return std::make_unique<proxy::ColumnData<SqlType::TEXT>>(program_, path);
+    case SqlType::BOOLEAN:
+      return std::make_unique<proxy::ColumnData<SqlType::BOOLEAN>>(program_,
+                                                                   path);
+  }
+}
+
+void ScanTranslator::Open() {
   const auto& table = scan_.Relation();
 
-  std::vector<std::unique_ptr<proxy::Iterable>> column_data_vars;
-  std::vector<std::unique_ptr<proxy::Iterable>> null_data_vars;
   const auto& cols = scan_.Schema().Columns();
   auto num_cols = cols.size();
-  column_data_vars.reserve(num_cols);
-  null_data_vars.reserve(num_cols);
+  column_data_vars_.reserve(num_cols);
+  null_data_vars_.reserve(num_cols);
   for (const auto& column : cols) {
-    using catalog::SqlType;
     auto type = column.Expr().Type();
     auto path = table[column.Name()].Path();
-    switch (type) {
-      case SqlType::SMALLINT:
-        column_data_vars.push_back(
-            std::make_unique<proxy::ColumnData<SqlType::SMALLINT>>(program_,
-                                                                   path));
-        break;
-      case SqlType::INT:
-        column_data_vars.push_back(
-            std::make_unique<proxy::ColumnData<SqlType::INT>>(program_, path));
-        break;
-      case SqlType::BIGINT:
-        column_data_vars.push_back(
-            std::make_unique<proxy::ColumnData<SqlType::BIGINT>>(program_,
-                                                                 path));
-        break;
-      case SqlType::REAL:
-        column_data_vars.push_back(
-            std::make_unique<proxy::ColumnData<SqlType::REAL>>(program_, path));
-        break;
-      case SqlType::DATE:
-        column_data_vars.push_back(
-            std::make_unique<proxy::ColumnData<SqlType::DATE>>(program_, path));
-        break;
-      case SqlType::TEXT:
-        column_data_vars.push_back(
-            std::make_unique<proxy::ColumnData<SqlType::TEXT>>(program_, path));
-        break;
-      case SqlType::BOOLEAN:
-        column_data_vars.push_back(
-            std::make_unique<proxy::ColumnData<SqlType::BOOLEAN>>(program_,
-                                                                  path));
-        break;
-    }
+
+    column_data_vars_.push_back(ConstructIterable(type, path));
 
     if (table[column.Name()].Nullable()) {
-      null_data_vars.push_back(
-          std::make_unique<proxy::ColumnData<SqlType::BOOLEAN>>(
-              program_, table[column.Name()].NullPath()));
+      null_data_vars_.push_back(ConstructIterable(
+          catalog::SqlType::BOOLEAN, table[column.Name()].NullPath()));
     } else {
-      null_data_vars.push_back(nullptr);
+      null_data_vars_.push_back(nullptr);
     }
   }
 
-  auto card_var = column_data_vars[0]->Size();
+  size_var_ = column_data_vars_[0]->Size().ToPointer();
+}
+
+proxy::SQLValue ScanTranslator::Get(int col_idx, proxy::Int32 tuple_idx) {
+  const auto& cols = scan_.Schema().Columns();
+  auto& column_data = *column_data_vars_[col_idx];
+  auto type = cols[col_idx].Expr().Type();
+  if (null_data_vars_[col_idx] == nullptr) {
+    return proxy::SQLValue(column_data[tuple_idx], type,
+                           proxy::Bool(program_, false));
+  } else {
+    auto& null_data = *null_data_vars_[col_idx];
+    auto null = null_data[tuple_idx];
+    return proxy::SQLValue(column_data[tuple_idx], type,
+                           static_cast<proxy::Bool&>(*null));
+  }
+}
+
+void ScanTranslator::Produce() {
+  Open();
 
   proxy::Loop loop(
       program_,
       [&](auto& loop) { loop.AddLoopVariable(proxy::Int32(program_, 0)); },
       [&](auto& loop) {
         auto i = loop.template GetLoopVariable<proxy::Int32>(0);
-        return i < card_var;
+        return i < *size_var_;
       },
       [&](auto& loop) {
         auto i = loop.template GetLoopVariable<proxy::Int32>(0);
 
         this->values_.ResetValues();
-        for (int k = 0; k < column_data_vars.size(); k++) {
-          auto& column_data = *column_data_vars[k];
-          auto type = cols[k].Expr().Type();
-          if (null_data_vars[k] == nullptr) {
-            this->values_.AddVariable(proxy::SQLValue(
-                column_data[i], type, proxy::Bool(program_, false)));
-          } else {
-            auto null = null_data_vars[k]->operator[](i);
-            this->values_.AddVariable(proxy::SQLValue(
-                column_data[i], type, static_cast<proxy::Bool&>(*null)));
-          }
+        for (int k = 0; k < column_data_vars_.size(); k++) {
+          this->values_.AddVariable(Get(k, i));
         }
 
         if (auto parent = this->Parent()) {
@@ -114,13 +113,44 @@ void ScanTranslator::Produce() {
         return loop.Continue(i + 1);
       });
 
-  for (auto& col : column_data_vars) {
+  Reset();
+}
+
+void ScanTranslator::Reset() {
+  for (auto& col : column_data_vars_) {
     col->Reset();
+  }
+  for (auto& col : null_data_vars_) {
+    if (col != nullptr) {
+      col->Reset();
+    }
   }
 }
 
+proxy::Int32 ScanTranslator::Size() { return *size_var_; }
+
 void ScanTranslator::Consume(OperatorTranslator& src) {
   throw std::runtime_error("Scan cannot consume tuples - leaf operator");
+}
+
+void ScanTranslator::Produce(int col_idx,
+                             std::function<void(const proxy::Int32& tuple_idx,
+                                                const proxy::SQLValue& value)>
+                                 handler) {
+  proxy::Loop loop(
+      program_,
+      [&](auto& loop) { loop.AddLoopVariable(proxy::Int32(program_, 0)); },
+      [&](auto& loop) {
+        auto i = loop.template GetLoopVariable<proxy::Int32>(0);
+        return i < *size_var_;
+      },
+      [&](auto& loop) {
+        auto i = loop.template GetLoopVariable<proxy::Int32>(0);
+
+        handler(i, Get(col_idx, i));
+
+        return loop.Continue(i + 1);
+      });
 }
 
 }  // namespace kush::compile
