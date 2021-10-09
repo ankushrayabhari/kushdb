077b7381d5552a0ea929ef843c42248e1b38d17c
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2021-10-09 14:49:48 -0400

    Revert "Add methods to ScanTranslator to support skinner opts"
    This reverts commit 4f39087ccf989173a62dff7f7202f8a954e94676.

--- compile/translators/scan_translator.h
+++ compile/translators/scan_translator.h
@@ -1,12 +1,8 @@
 #pragma once
 
-#include <functional>
-
 #include "absl/container/flat_hash_map.h"
 
 #include "compile/proxy/column_data.h"
-#include "compile/proxy/value/ir_value.h"
-#include "compile/proxy/value/sql_value.h"
 #include "compile/translators/operator_translator.h"
 #include "khir/program_builder.h"
 #include "plan/scan_operator.h"
@@ -22,23 +18,7 @@ class ScanTranslator : public OperatorTranslator {
   void Produce() override;
   void Consume(OperatorTranslator& src) override;
 
-  void Open();
-  void Produce(int col_idx, std::function<void(const proxy::Int32& tuple_idx,
-                                               const proxy::SQLValue& value)>
-                                handler);
-
-  proxy::Int32 Size();
-  proxy::SQLValue Get(int col_idx, proxy::Int32 i);
-  void Reset();
-
  private:
-  std::vector<std::unique_ptr<proxy::Iterable>> column_data_vars_;
-  std::vector<std::unique_ptr<proxy::Iterable>> null_data_vars_;
-  std::unique_ptr<proxy::Int32> size_var_;
-
-  std::unique_ptr<proxy::Iterable> ConstructIterable(catalog::SqlType type,
-                                                     std::string_view path);
-
   const plan::ScanOperator& scan_;
   khir::ProgramBuilder& program_;
 };
