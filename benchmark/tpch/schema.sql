c5a8ad4601672eddfaa2eca007ac673958ccf1ac
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2021-10-14 17:36:22 -0400

    Move primary key constraints into table definition

--- benchmark/tpch/schema.sql
+++ benchmark/tpch/schema.sql
@@ -1,3 +1,18 @@
+CREATE TABLE region (
+    r_regionkey INTEGER NOT NULL,
+    r_name TEXT NOT NULL,
+    r_comment TEXT NOT NULL,
+    PRIMARY KEY (r_regionkey)
+);
+
+CREATE TABLE nation (
+    n_nationkey INTEGER NOT NULL,
+    n_name TEXT NOT NULL,
+    n_regionkey INTEGER NOT NULL,
+    n_comment TEXT NOT NULL,
+    PRIMARY KEY (n_nationkey),
+);
+
 CREATE TABLE supplier (
     s_suppkey  INTEGER NOT NULL,
     s_name TEXT NOT NULL,
@@ -5,7 +20,8 @@ CREATE TABLE supplier (
     s_nationkey INTEGER NOT NULL,
     s_phone TEXT NOT NULL,
     s_acctbal DOUBLE PRECISION NOT NULL,
-    s_comment TEXT NOT NULL
+    s_comment TEXT NOT NULL,
+    PRIMARY KEY (s_suppkey)
 );
 
 CREATE TABLE part (
@@ -17,7 +33,8 @@ CREATE TABLE part (
     p_size INTEGER NOT NULL,
     p_container TEXT NOT NULL,
     p_retailprice DOUBLE PRECISION NOT NULL,
-    p_comment TEXT NOT NULL
+    p_comment TEXT NOT NULL,
+    PRIMARY KEY (p_partkey)
 );
 
 CREATE TABLE partsupp (
@@ -25,7 +42,8 @@ CREATE TABLE partsupp (
     ps_suppkey INTEGER NOT NULL,
     ps_availqty INTEGER NOT NULL,
     ps_supplycost DOUBLE PRECISION NOT NULL,
-    ps_comment TEXT NOT NULL
+    ps_comment TEXT NOT NULL,
+    PRIMARY KEY (ps_partkey, ps_suppkey)
 );
 
 CREATE TABLE customer (
@@ -36,7 +54,8 @@ CREATE TABLE customer (
     c_phone TEXT NOT NULL,
     c_acctbal DOUBLE PRECISION NOT NULL,
     c_mktsegment TEXT NOT NULL,
-    c_comment TEXT NOT NULL
+    c_comment TEXT NOT NULL,
+    PRIMARY KEY (c_custkey)
 );
 
 CREATE TABLE orders (
@@ -48,7 +67,8 @@ CREATE TABLE orders (
     o_orderpriority TEXT NOT NULL,
     o_clerk TEXT NOT NULL,
     o_shippriority INTEGER NOT NULL,
-    o_comment TEXT NOT NULL
+    o_comment TEXT NOT NULL,
+    PRIMARY KEY (o_orderkey)
 );
 
 CREATE TABLE lineitem (
@@ -67,31 +87,10 @@ CREATE TABLE lineitem (
     l_receiptdate TIMESTAMP NOT NULL,
     l_shipinstruct TEXT NOT NULL,
     l_shipmode TEXT NOT NULL,
-    l_comment TEXT NOT NULL
+    l_comment TEXT NOT NULL,
+    PRIMARY KEY (l_orderkey, l_linenumber)
 );
 
-CREATE TABLE nation (
-    n_nationkey INTEGER NOT NULL,
-    n_name TEXT NOT NULL,
-    n_regionkey INTEGER NOT NULL,
-    n_comment TEXT NOT NULL
-);
-
-CREATE TABLE region (
-    r_regionkey INTEGER NOT NULL,
-    r_name TEXT NOT NULL,
-    r_comment TEXT NOT NULL
-);
-
-ALTER TABLE region   ADD CONSTRAINT region_pk    PRIMARY KEY (r_regionkey);
-ALTER TABLE nation   ADD CONSTRAINT nation_pk    PRIMARY KEY (n_nationkey);
-ALTER TABLE part     ADD CONSTRAINT part_pk      PRIMARY KEY (p_partkey);
-ALTER TABLE supplier ADD CONSTRAINT supplier_pk  PRIMARY KEY (s_suppkey);
-ALTER TABLE partsupp ADD CONSTRAINT partsupp_pk  PRIMARY KEY (ps_partkey, ps_suppkey);
-ALTER TABLE customer ADD CONSTRAINT customer_pk  PRIMARY KEY (c_custkey);
-ALTER TABLE lineitem ADD CONSTRAINT lineitem_pk  PRIMARY KEY (l_orderkey, l_linenumber);
-ALTER TABLE orders   ADD CONSTRAINT orders_pk    PRIMARY KEY (o_orderkey);
-ALTER TABLE nation   ADD CONSTRAINT nation_fk    FOREIGN KEY (n_regionkey) REFERENCES region(r_regionkey);
 ALTER TABLE supplier ADD CONSTRAINT supplier_fk  FOREIGN KEY (s_nationkey) REFERENCES nation(n_nationkey);
 ALTER TABLE customer ADD CONSTRAINT customer_fk  FOREIGN KEY (c_nationkey) REFERENCES nation(n_nationkey);
 ALTER TABLE partsupp ADD CONSTRAINT partsupp_fk1 FOREIGN KEY (ps_suppkey) REFERENCES supplier(s_suppkey);
