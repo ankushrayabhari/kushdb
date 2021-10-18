8f37e3c36c1f74875db27892cd045fe0199769e6
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2021-10-18 11:27:57 -0400

    Add nation to hyper load

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Compilation-based execution engine for a database
 1. Run [DBGEN](https://github.com/electrum/tpch-dbgen) to generate .tbl files.
 2. Move them to `benchmark/tpch/raw-1/` for scale factor 1 and
    `benchmark/tpch/raw-10` for scale factor 10.
-3. Execute: `for i in benchmark/tpch/raw-1/*.tbl benchmark/tpch/raw-10/*.tbl; do sed -i 's/.$//' $i > $i.tight; done`.
+3. Execute: `for i in benchmark/tpch/raw-1/*.tbl benchmark/tpch/raw-10/*.tbl; do sed 's/.$//' $i > $i.tight; done`.
 
 #### KushDB
 1. Run `bazel build //benchmark/tpch:load`.
--- benchmark/tpch/hyper_load.cc
+++ benchmark/tpch/hyper_load.cc
@@ -4,7 +4,7 @@
 
 #include "hyperapi/hyperapi.hpp"
 
-static const hyperapi::TableDefinition region{
+const hyperapi::TableDefinition region{
     "region",
     {
         hyperapi::TableDefinition::Column{"r_regionkey",
@@ -17,8 +17,8 @@ static const hyperapi::TableDefinition region{
                                           hyperapi::Nullability::NotNullable},
     }};
 
-static void Region(hyperapi::HyperProcess& hyper, std::string dest_path,
-                   std::string raw) {
+void Region(hyperapi::HyperProcess& hyper, std::string dest_path,
+            std::string raw) {
   {
     hyperapi::Connection connection(hyper.getEndpoint(), dest_path,
                                     hyperapi::CreateMode::CreateAndReplace);
@@ -27,7 +27,7 @@ static void Region(hyperapi::HyperProcess& hyper, std::string dest_path,
 
     // Using path to current file, create a path that locates CSV file
     // packaged with these examples.
-    std::string csv_path = raw + "region.tbl";
+    std::string csv_path = raw + "region.tbl.tight";
 
     int64_t rowCount = connection.executeCommand(
         "COPY " + region.getTableName().toString() + " FROM " +
@@ -39,18 +39,120 @@ static void Region(hyperapi::HyperProcess& hyper, std::string dest_path,
   }
 }
 
-int main(int argc, const char** argv) {
-  auto dest_path = "benchmark/tpch/hyper/tpch1.hyper";
-  auto raw_path = "benchmark/tpch/raw-1/";
+const hyperapi::TableDefinition nation{
+    "nation",
+    {
+        hyperapi::TableDefinition::Column{"n_nationkey",
+                                          hyperapi::SqlType::integer(),
+                                          hyperapi::Nullability::NotNullable},
+        hyperapi::TableDefinition::Column{"n_name", hyperapi::SqlType::text(),
+                                          hyperapi::Nullability::NotNullable},
+        hyperapi::TableDefinition::Column{"n_regionkey",
+                                          hyperapi::SqlType::integer(),
+                                          hyperapi::Nullability::NotNullable},
+        hyperapi::TableDefinition::Column{"n_comment",
+                                          hyperapi::SqlType::text(),
+                                          hyperapi::Nullability::NotNullable},
+    }};
 
-  try {
-    hyperapi::HyperProcess hyper(
-        "bazel-bin/benchmark/tpch/hyper_load.runfiles/hyper/lib/hyper/",
-        hyperapi::Telemetry::DoNotSendUsageDataToTableau);
-    Region(hyper, dest_path, raw_path);
-  } catch (const hyperapi::HyperException& e) {
-    std::cerr << e.toString() << std::endl;
-    return 1;
+void Nation(hyperapi::HyperProcess& hyper, std::string dest_path,
+            std::string raw) {
+  {
+    hyperapi::Connection connection(hyper.getEndpoint(), dest_path,
+                                    hyperapi::CreateMode::CreateAndReplace);
+    const hyperapi::Catalog& catalog = connection.getCatalog();
+    catalog.createTable(nation);
+
+    // Using path to current file, create a path that locates CSV file
+    // packaged with these examples.
+    std::string csv_path = raw + "nation.tbl.tight";
+
+    int64_t rowCount = connection.executeCommand(
+        "COPY " + nation.getTableName().toString() + " FROM " +
+        hyperapi::escapeStringLiteral(csv_path) +
+        " WITH (FORMAT CSV, DELIMITER '|')");
+
+    std::cout << "Finished " << nation.getTableName() << ": " << rowCount
+              << std::endl;
+  }
+}
+
+const hyperapi::TableDefinition part{
+    "part",
+    {
+        hyperapi::TableDefinition::Column{"p_partkey",
+                                          hyperapi::SqlType::integer(),
+                                          hyperapi::Nullability::NotNullable},
+        hyperapi::TableDefinition::Column{"p_name", hyperapi::SqlType::text(),
+                                          hyperapi::Nullability::NotNullable},
+        hyperapi::TableDefinition::Column{"p_mfgr", hyperapi::SqlType::text(),
+                                          hyperapi::Nullability::NotNullable},
+        hyperapi::TableDefinition::Column{"p_brand", hyperapi::SqlType::text(),
+                                          hyperapi::Nullability::NotNullable},
+        hyperapi::TableDefinition::Column{"p_type", hyperapi::SqlType::text(),
+                                          hyperapi::Nullability::NotNullable},
+        hyperapi::TableDefinition::Column{"p_size",
+                                          hyperapi::SqlType::integer(),
+                                          hyperapi::Nullability::NotNullable},
+        hyperapi::TableDefinition::Column{"p_container",
+                                          hyperapi::SqlType::text(),
+                                          hyperapi::Nullability::NotNullable},
+        hyperapi::TableDefinition::Column{"p_retailprice",
+                                          hyperapi::SqlType::doublePrecision(),
+                                          hyperapi::Nullability::NotNullable},
+        hyperapi::TableDefinition::Column{"p_comment",
+                                          hyperapi::SqlType::text(),
+                                          hyperapi::Nullability::NotNullable},
+    }};
+
+void Part(hyperapi::HyperProcess& hyper, std::string dest_path,
+          std::string raw) {
+  {
+    hyperapi::Connection connection(hyper.getEndpoint(), dest_path,
+                                    hyperapi::CreateMode::CreateAndReplace);
+    const hyperapi::Catalog& catalog = connection.getCatalog();
+    catalog.createTable(part);
+
+    // Using path to current file, create a path that locates CSV file
+    // packaged with these examples.
+    std::string csv_path = raw + "part.tbl.tight";
+
+    int64_t rowCount = connection.executeCommand(
+        "COPY " + part.getTableName().toString() + " FROM " +
+        hyperapi::escapeStringLiteral(csv_path) +
+        " WITH (FORMAT CSV, DELIMITER '|')");
+
+    std::cout << "Finished " << part.getTableName() << ": " << rowCount
+              << std::endl;
+  }
+}
+
+int main(int argc, const char** argv) {
+  auto hyper_path =
+      "bazel-bin/benchmark/tpch/hyper_load.runfiles/hyper/lib/hyper/";
+
+  std::vector<std::pair<std::string, std::string>> databases{
+      {"benchmark/tpch/hyper/tpch1.hyper", "benchmark/tpch/raw-1/"},
+      {"benchmark/tpch/hyper/tpch10.hyper", "benchmark/tpch/raw-10/"},
+  };
+
+  for (const auto& [dest_path, raw_path] : databases) {
+    std::cout << "Working on " << raw_path << std::endl;
+    try {
+      hyperapi::HyperProcess hyper(
+          hyper_path, hyperapi::Telemetry::DoNotSendUsageDataToTableau);
+      Region(hyper, dest_path, raw_path);
+      Nation(hyper, dest_path, raw_path);
+      Part(hyper, dest_path, raw_path);
+      // Supplier(hyper, dest_path, raw_path);
+      // Partsupp(hyper, dest_path, raw_path);
+      // Customer(hyper, dest_path, raw_path);
+      // Orders(hyper, dest_path, raw_path);
+      // Lineitem(hyper, dest_path, raw_path);
+    } catch (const hyperapi::HyperException& e) {
+      std::cerr << e.toString() << std::endl;
+      return 1;
+    }
   }
   return 0;
 }
