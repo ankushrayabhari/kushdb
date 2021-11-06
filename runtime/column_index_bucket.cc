ddb7efef22741270d17986d97c2f47233ae0948c
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2021-11-06 15:50:04 -0400

    Add iterative sorted intersection of indexes

--- runtime/column_index_bucket.cc
+++ runtime/column_index_bucket.cc
@@ -1,6 +1,10 @@
 #include "runtime/column_index_bucket.h"
 
 #include <cstdint>
+#include <functional>
+#include <queue>
+#include <utility>
+#include <vector>
 
 namespace kush::runtime {
 
@@ -35,6 +39,38 @@ int32_t GetBucketValue(ColumnIndexBucket* bucket, int32_t idx) {
 
 ColumnIndexBucket* BucketListGet(ColumnIndexBucket* bucket, int32_t idx) {
   return &bucket[idx];
+}
+
+int32_t BucketListSortedIntersection(ColumnIndexBucket* bucket_list,
+                                     int32_t bucket_list_size,
+                                     int32_t* bucket_list_idx, int32_t* result,
+                                     int32_t result_max_size) {
+  using val_listidx_t = std::pair<int, int>;
+  auto comp = [](const val_listidx_t& a, const val_listidx_t& b) {
+    return a.first > b.first;
+  };
+  std::priority_queue<val_listidx_t, std::vector<val_listidx_t>, decltype(comp)>
+      heap(comp);
+
+  for (int i = 0; i < bucket_list_size; i++) {
+    auto next_idx = bucket_list_idx[i];
+    if (next_idx < bucket_list[i].size) {
+      heap.emplace(bucket_list[i].data[next_idx], i);
+    }
+  }
+
+  int32_t result_size = 0;
+  while (!heap.empty() && result_size < result_max_size) {
+    auto [min_val, min_list_idx] = heap.top();
+    heap.pop();
+
+    result[result_size++] = min_val;
+    auto next_idx = ++bucket_list_idx[min_list_idx];
+    if (next_idx < bucket_list[min_list_idx].size) {
+      heap.emplace(bucket_list[min_list_idx].data[next_idx], min_list_idx);
+    }
+  }
+  return result_size;
 }
 
 }  // namespace kush::runtime
--- runtime/column_index_bucket.h
+++ runtime/column_index_bucket.h
@@ -13,6 +13,10 @@ int32_t FastForwardBucket(ColumnIndexBucket* bucket, int32_t prev_tuple);
 
 int32_t GetBucketValue(ColumnIndexBucket* bucket, int32_t idx);
 
-ColumnIndexBucket* BucketListGet(ColumnIndexBucket* bucket, int32_t idx);
+ColumnIndexBucket* BucketListGet(ColumnIndexBucket* bucket_list, int32_t idx);
+
+int32_t BucketListSortedIntersection(ColumnIndexBucket* bucket_list,
+                                     int32_t size, int32_t* intersection_state,
+                                     int32_t* result, int32_t result_max_size);
 
 }  // namespace kush::runtime
