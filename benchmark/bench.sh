7a84f2f39b02c00f800510d44e72d6ea643c7821
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2022-04-25 17:02:26 -0400

    Update bench.sh

--- benchmark/bench.sh
+++ benchmark/bench.sh
@@ -3,3 +3,4 @@ python benchmark/tpch1/bench.py > tpch.csv
 python benchmark/tpch10/bench.py >> tpch.csv
 python benchmark/jcch1/bench.py > jcch.csv
 python benchmark/jcch10/bench.py >> jcch.csv
+python benchmark/job/bench.py > job.csv
