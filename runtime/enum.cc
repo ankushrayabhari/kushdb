eeb5c9b484dcb0c37811cb4a0dd2e0362afca0cd
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2022-05-01 14:06:52 -0400

    Implement enum manager

---
+++ runtime/enum.cc
@@ -0,0 +1,42 @@
+#include "runtime/enum.h"
+
+#include <cstdint>
+#include <deque>
+#include <memory>
+
+#include "absl/container/flat_hash_map.h"
+
+#include "runtime/column_data.h"
+#include "runtime/string.h"
+
+namespace kush::runtime {
+
+void EnumManager::Register(int32_t id, std::string key_path,
+                           std::string map_path) {
+  auto& data = info_[id];
+  ColumnData::OpenText(&data.keys, key_path.c_str());
+  ColumnIndex::Open(&data.map, map_path.c_str());
+}
+
+void EnumManager::GetKey(int32_t id, int32_t value, String::String* dest) {
+  auto& data = info_.at(id);
+  ColumnData::GetText(&data.keys, value, dest);
+}
+
+int32_t EnumManager::GetValue(int32_t id, std::string_view value) {
+  auto& data = info_.at(id);
+  String::String key{.data = value.data(), .length = (int32_t)value.size()};
+  ColumnIndexBucket dest;
+  ColumnIndex::GetText(&data.map, &key, &dest);
+  return dest.data[0];
+}
+
+void GetKey(int32_t id, int32_t value, String::String* dest) {
+  EnumManager::Get().GetKey(id, value, dest);
+}
+
+int32_t GetValue(int32_t id, std::string_view value) {
+  return EnumManager::Get().GetValue(id, value);
+}
+
+}  // namespace kush::runtime
---
+++ runtime/enum.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <cstdint>
+#include <deque>
+#include <memory>
+
+#include "absl/container/flat_hash_map.h"
+
+#include "runtime/column_data.h"
+#include "runtime/column_index.h"
+#include "runtime/string.h"
+
+namespace kush::runtime {
+
+struct EnumData {
+  ColumnData::TextColumnData keys;
+  ColumnIndex::ColumnIndex map;
+};
+
+class EnumManager {
+ public:
+  static EnumManager& Get() {
+    static EnumManager instance;
+    return instance;
+  }
+
+ private:
+  EnumManager() = default;
+  ~EnumManager();
+
+ public:
+  EnumManager(EnumManager const&) = delete;
+  void operator=(EnumManager const&) = delete;
+
+  void Register(int32_t id, std::string key_path, std::string map_path);
+  void GetKey(int32_t id, int32_t value, String::String* dest);
+  int32_t GetValue(int32_t id, std::string_view value);
+
+ private:
+  absl::flat_hash_map<int32_t, EnumData> info_;
+};
+
+void GetKey(int32_t id, int32_t value, String::String* dest);
+int32_t GetValue(int32_t id, std::string_view value);
+
+}  // namespace kush::runtime
