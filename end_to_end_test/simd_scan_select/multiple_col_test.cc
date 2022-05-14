232b645f168fcdd52db07344cb377c2a2e703223
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2022-05-14 12:04:31 -0400

    Add multiple col filters test for simd scan select

---
+++ end_to_end_test/simd_scan_select/multiple_col_test.cc
@@ -0,0 +1,97 @@
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include "catalog/catalog.h"
+#include "catalog/sql_type.h"
+#include "compile/query_translator.h"
+#include "end_to_end_test/parameters.h"
+#include "end_to_end_test/schema.h"
+#include "plan/expression/aggregate_expression.h"
+#include "plan/expression/arithmetic_expression.h"
+#include "plan/expression/column_ref_expression.h"
+#include "plan/expression/literal_expression.h"
+#include "plan/expression/virtual_column_ref_expression.h"
+#include "plan/operator/group_by_aggregate_operator.h"
+#include "plan/operator/hash_join_operator.h"
+#include "plan/operator/operator.h"
+#include "plan/operator/operator_schema.h"
+#include "plan/operator/order_by_operator.h"
+#include "plan/operator/output_operator.h"
+#include "plan/operator/scan_operator.h"
+#include "plan/operator/scan_select_operator.h"
+#include "plan/operator/select_operator.h"
+#include "plan/operator/simd_scan_select_operator.h"
+#include "util/builder.h"
+#include "util/test_util.h"
+
+using namespace kush;
+using namespace kush::util;
+using namespace kush::plan;
+using namespace kush::compile;
+using namespace kush::catalog;
+using namespace std::literals;
+
+class SelectTest : public testing::TestWithParam<ParameterValues> {};
+
+TEST_P(SelectTest, TextCol) {
+  SetFlags(GetParam());
+
+  auto db = EnumSchema();
+
+  std::unique_ptr<Operator> query;
+  {
+    OperatorSchema scan_schema;
+    scan_schema.AddGeneratedColumns(db["lineitem"],
+                                    {"l_shipdate", "l_returnflag"});
+
+    std::vector<std::vector<std::unique_ptr<BinaryArithmeticExpression>>>
+        filters(2);
+    filters[0].push_back(
+        Leq(VirtColRef(scan_schema, "l_shipdate"), Literal(1994, 12, 31)));
+    filters[0].push_back(
+        Geq(VirtColRef(scan_schema, "l_shipdate"), Literal(1994, 1, 1)));
+    filters[1].push_back(
+        Eq(VirtColRef(scan_schema, "l_returnflag"), Literal("R"sv)));
+
+    OperatorSchema schema;
+    schema.AddVirtualPassthroughColumns(scan_schema,
+                                        {"l_shipdate", "l_returnflag"});
+    query = std::make_unique<OutputOperator>(
+        std::make_unique<SimdScanSelectOperator>(
+            std::move(schema), std::move(scan_schema), db["lineitem"],
+            std::move(filters)));
+  }
+
+  auto expected_file =
+      "end_to_end_test/simd_scan_select/multiple_col_expected.tbl";
+  auto output_file = ExecuteAndCapture(*query);
+
+  auto expected = GetFileContents(expected_file);
+  auto output = GetFileContents(output_file);
+  std::sort(expected.begin(), expected.end());
+  std::sort(output.begin(), output.end());
+
+  EXPECT_EQ(output, expected);
+}
+
+INSTANTIATE_TEST_SUITE_P(ASMBackend_StackSpill, SelectTest,
+                         testing::Values(ParameterValues{
+                             .backend = "asm",
+                             .reg_alloc = "stack_spill",
+                         }));
+
+INSTANTIATE_TEST_SUITE_P(ASMBackend_LinearScan, SelectTest,
+                         testing::Values(ParameterValues{
+                             .backend = "asm",
+                             .reg_alloc = "linear_scan",
+                         }));
+
+INSTANTIATE_TEST_SUITE_P(LLVMBackend, SelectTest,
+                         testing::Values(ParameterValues{
+                             .backend = "llvm",
+                         }));
